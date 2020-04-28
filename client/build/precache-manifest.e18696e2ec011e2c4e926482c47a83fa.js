43aede6afc952aacb384c9ea8b2b6d2c5a920c3b
Julie T <joules2319@gmail.com>
2020-04-27 18:32:53 -0600

    updated build

---
+++ client/build/precache-manifest.e18696e2ec011e2c4e926482c47a83fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d48255e232b02774c23",
+    "url": "/static/css/main.b7b8e30e.chunk.css"
+  },
+  {
+    "revision": "3d48255e232b02774c23",
+    "url": "/static/js/main.29d5e7f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "f7fa2691a65cf8acdd7629ea4944d4ee",
+    "url": "/index.html"
+  }
+];
