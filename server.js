989b2510969e707f0bb4428955136330d9da9bec
Julie T <joules2319@gmail.com>
2020-04-29 19:42:06 -0600

    adjusted producting api get default path

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ mongoose
 // Serve static assets (build folder) if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
